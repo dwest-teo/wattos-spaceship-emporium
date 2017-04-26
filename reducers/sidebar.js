dd1759e7ac6c445297e23796e6f2e58d64adae91
Donny West <donnywest@gmail.com>
2017-04-26 11:03:09 -0400

    clean up sidebar reducer comments

--- reducers/sidebar.js
+++ reducers/sidebar.js
@@ -1,17 +1,11 @@
 import { handleActions } from 'redux-actions';
-// import { CALCULATE_RESPONSIVE_STATE } from 'redux-responsive';
 import { SidebarActions } from '../actions';
 
 const sidebarState = {
-  // docked: false,
   open: false,
 };
 
 const Sidebar = handleActions({
-  // [CALCULATE_RESPONSIVE_STATE]: (state, action) => ({
-    // ...state,
-    // docked: action.innerWidth >= 832,
-  // }),
   [SidebarActions.openSidebar]: (state, action) => ({
     ...state,
     open: action.payload,
