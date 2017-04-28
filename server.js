bbde2164b8c488d78dde8c7adb53fbd458945b94
Donny West <donnywest@gmail.com>
2017-04-27 20:33:33 -0400

    update start scripts

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const { createServer } = require('http');
 const next = require('next');
 const routes = require('./routes');
 
-const app = next({ dev: process.env.NODE_ENV !== 'production' });
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
 const handler = routes.getRequestHandler(app);
 
 app.prepare().then(() => {
