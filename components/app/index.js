5235612ee655e4e8e1244bdaad497480abfe9f4c
Donny West <donnywest@gmail.com>
2017-04-29 01:54:14 -0400

    add GA

--- components/app/index.js
+++ components/app/index.js
@@ -1,56 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
+import { initAnalytics, trackPageView } from '../../lib/analytics';
 import { Container } from '../base';
 import Sidebar from '../sidebar';
 import CartDropdown from '../cart-dropdown';
 import Content from './content';
 
-const defaultTitle = 'Watto\'s Spaceship Emporium';
-const pageTitle = title => title ? `${title} - ${defaultTitle}` : defaultTitle;
-
+initAnalytics();
+const pageTitle = title => title ? `Watto's | ${title}` : `Watto's Spaceship Emporium`;
 const makeLinks = products => products.map(p => ({ name: p.name, slug: p.slug }));
 
-const App = (props) => {
-  const {
-    children,
-    products,
-    cartProducts,
-    activeLink,
-    isSidebarOpen,
-    isDropdownOpen,
-    isLarge,
-    title,
-    toggleDropdown,
-    removeFromCart,
-  } = props;
+class App extends Component {
+  componentWillMount() {
+    trackPageView();
+  }
 
-  return (
-    <Container p0 css={{ minHeight: '100vh' }}>
-      <Head>
-        <title>{pageTitle(title)}</title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <Sidebar
-        open={isLarge || isSidebarOpen}
-        productLinks={makeLinks(products)}
-        activeLink={activeLink}
-        onDismiss={() => props.openSidebar(false)}
-      />
-      <Content openSidebar={props.openSidebar} {...props}>
-        {children}
-      </Content>
-      <CartDropdown
-        cartProducts={cartProducts}
-        open={isDropdownOpen}
-        isLarge={isLarge}
-        toggleDropdown={toggleDropdown}
-        removeFromCart={removeFromCart}
-      />
-    </Container>
-  );
-};
+  render() {
+    const {
+      children,
+      products,
+      cartProducts,
+      activeLink,
+      isSidebarOpen,
+      isDropdownOpen,
+      isLarge,
+      title,
+      toggleDropdown,
+      removeFromCart,
+    } = this.props;
+
+    return (
+      <Container p0 css={{ minHeight: '100vh' }}>
+        <Head>
+          <title>{pageTitle(title)}</title>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
+        <Sidebar
+          open={isLarge || isSidebarOpen}
+          productLinks={makeLinks(products)}
+          activeLink={activeLink}
+          onDismiss={() => this.props.openSidebar(false)}
+        />
+        <Content openSidebar={this.props.openSidebar} {...this.props}>
+          {children}
+        </Content>
+        <CartDropdown
+          cartProducts={cartProducts}
+          open={isDropdownOpen}
+          isLarge={isLarge}
+          toggleDropdown={toggleDropdown}
+          removeFromCart={removeFromCart}
+        />
+      </Container>
+    );
+  }
+}
 
 App.propTypes = {
   products: PropTypes.array,
